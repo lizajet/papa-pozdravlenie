ad4654d9829610861cd3a7cfa946d015ed850583
lizajet <liza@jetstyle.ru>
2026-09-24 17:55:16 +0500

    Fix runner facing direction

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -193,7 +193,7 @@ export function Experience() {
         >
           <span
             className="runner__sprite"
-            style={{ backgroundImage: `url(${assetUrl("/characters/papa-runner-cycle-v5.png")})` }}
+            style={{ backgroundImage: `url(${assetUrl("/characters/papa-runner-cycle-v3.png")})` }}
             aria-hidden="true"
           />
         </div>
